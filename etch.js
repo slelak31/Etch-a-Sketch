301344a314dc71110e821fbfef7b3ac9008f5271
Selmir Lelak <Lelaks@mail.gvsu.edu>
2025-11-05 00:45:11 -0500

    added gridsize dynamic variables and also mousedown fillcolor function

--- etch.js
+++ etch.js
@@ -1,16 +1,36 @@
-const defaultGridSize = 16;
+let GridSize = 16;
+
+let drawingActive = false;
+
+let rainbowActive = false;
+
+let eraserActive = false;
+
+let currentColor = 'black';
+
 
 const container = document.querySelector(".container");
 
+container.style.setProperty('--size', GridSize);
+
+function fillColor (cell) {
+    cell.style.backgroundColor = currentColor;
+};
+
+container.addEventListener('mousedown', (e) => {
+    drawingActive = true;
+    if (e.target.matches('.grid-square')) {
+        fillColor(e.target);
+    }
+    e.preventDefault();
+});
+
 let gridDiv = [];
 
-for (let i = 0; i < (defaultGridSize * defaultGridSize); i++) {
+for (let i = 0; i < (GridSize * GridSize); i++) {
     gridDiv[i] = document.createElement("div")
     gridDiv[i].classList.add("grid-square")
-}
-
-for(let j = 0; j < defaultGridSize * defaultGridSize; j++) {
-container.appendChild(gridDiv[j]);
+    container.appendChild(gridDiv[i]);
 }
 
 
